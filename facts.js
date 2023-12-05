48bed8ce16267c905ae68257313fdc81febfb6d8
Nikhil <nikhilraikwar846@gmail.com>
2023-12-05 10:28:20 +0530

    new visual facts feature and now user can contact us

---
+++ facts.js
@@ -0,0 +1,70 @@
+let k=0;
+let i=0;
+var t=0;
+var sea=document.getElementById(cardContainer);
+let parentElement = document.getElementById('cardContainer');
+var query = "aqi";
+  function search(){
+        parentElement.innerHTML="";
+     query = document.getElementById("search-input").value;
+    console.log(query); 
+    checknews(query);
+  }
+  
+sea ="";
+var cardData = [
+    { title: "Card " + t, content: "This is the content of Card 1." },
+];
+  function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+  }
+
+const key="4fe360bea1754332b25594ca6cc98ccc";
+const api = "https://newsapi.org/v2/everything?q="+`${query}`+"&apiKey=4fe360bea1754332b25594ca6cc98ccc";
+async function checknews(query){
+    sea.innerHTML="";
+    const api = "https://newsapi.org/v2/everything?q="+`${query}`+"&apiKey=4fe360bea1754332b25594ca6cc98ccc";
+    const response = await fetch(api + `&appid=${key}`);
+    var data = await response.json();
+    console.log(data);
+    console.log(data.articles.length)
+    k=data.articles.length;
+    for(i=0;i<k;i++){
+        if(data.articles[i].description==null){
+            continue;
+        }
+    cardData = [
+            { title: data.articles[i].title, content: data.articles[i].description ,image: data.articles[i].urlToImage},
+        ];
+        addCardsToContainer(cardData);
+    t++;
+    }
+}
+console.log(k);
+    function createCard(title, content,image) {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="${image}" id="img">
+            <h3>${title}</h3>
+            <p>${content}</p>
+        `;
+        return card;
+    }
+
+    function addCardsToContainer(data) {
+        const cardContainer = document.getElementById("cardContainer");
+        data.forEach(item => {
+            const card = createCard(item.title, item.content,item.image);
+            cardContainer.appendChild(card);
+        });
+
+    }
+    // Add cards to the container
+    checknews(query);
+  
--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 
-
 function GetInfo() {
     var newName = document.getElementById("cityInput");
     var cityName = document.getElementById("cityName");
     cityName.innerHTML = "--"+newName.value+"--";
    
+
+    async function fetchData() {
+        try {
+            const response = await fetch('https://api.waqi.info/feed/'+newName.value+'/?token=362fd0e01d4f28069afd4feb9319b9545125d3e5');
+            const data = await response.json();
+            return data.data.aqi;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
+
+    function updateAQIRepresentation(aqi) {
+        const aqiCircle = document.getElementById('aqi-circle');
+
+        aqiCircle.textContent = aqi;
+
+        if (aqi <= 50) {
+            aqiCircle.className = 'good';
+        } else if (aqi <= 100) {
+            aqiCircle.className = 'moderate';
+        } else {
+            aqiCircle.className = 'unhealthy';
+        }
+    }
+
+    async function updateAQI() {
+        try {
+            const aqi = await fetchData();
+            updateAQIRepresentation(aqi);
+        } catch (error) {
+            console.error('Error updating AQI:', error);
+        }
+    }
+
+    updateAQI();
+
+    setInterval(updateAQI, 5 * 60 * 1000);
+
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=ce43e15daba37b83f3c9290def769506')
 .then(response => response.json())
 .then(data => {
@@ -14,7 +51,6 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
         document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
   
     }
-
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
     }
@@ -56,5 +92,4 @@ function CheckDay(day){
     for(i = 0; i<5; i++){
         document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
     }
-
-    
+            
